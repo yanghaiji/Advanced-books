b1a24ea912355aac6ae922effecfc751519987ec
haiji.yang <haiji.yang@capgemini.com>
2020-10-15 12:13:28 +0800

    add 内存与垃圾回收篇 :new: :book:

---
+++ source-code/src/main/java/com/javayh/advanced/java/algorithm/example/sort/插入排序.java
@@ -0,0 +1,30 @@
+package com.javayh.advanced.java.algorithm.example.sort;
+
+import java.util.Arrays;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2020-10-13
+ */
+public class 插入排序 {
+
+    private static final int[] array =
+            {49, 38, 65, 97, 76, 13, 27, 78, 34, 12, 15, 35, 25, 53, 51};
+    public static void main(String[] args) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            for (; j >= 0 && array[j] > temp; j--) {
+                //将大于temp的值整体后移一个单位
+                array[j + 1] = array[j];
+            }
+            array[j + 1] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
